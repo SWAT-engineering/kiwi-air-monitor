3e8200d3806e6d0c02f6811edd44234ecf731dd5
Davy Landman <davy.landman@swat.engineering>
2020-11-05 20:33:20 +0100

    Switch display to always show all values and plot latest half hour, except if no movement detected

--- device/software/include/output/display.hpp
+++ device/software/include/output/display.hpp
@@ -4,7 +4,8 @@
 #include "sensors/sensors.hpp"
 #include "shared-timer.hpp"
 
-enum DisplayState: unsigned int;
+constexpr u8g2_uint_t PLOT_SIZE = 32;
+constexpr unsigned long PIXEL_DURATION = 60 * 1000;
 
 class Display {
 public:
@@ -13,10 +14,14 @@ public:
 
 private:
     bool render();
+    bool updateValues();
     U8G2 *u8g2;
     Sensors *source;
-    DisplayState state;
     bool visible;
-    uint16_t screenShownCount;
-    void renderScreen(DisplayState state);
+    bool filled;
+    uint16_t minutesPosition;
+    double co2Values[PLOT_SIZE];
+    void renderScreen();
+    void displayValues();
+    void plotGraph();
 };
--- device/software/src/output/display.cpp
+++ device/software/src/output/display.cpp
@@ -1,35 +1,8 @@
 #include "output/display.hpp"
 #include "output/logo.h"
 
-enum DisplayState: unsigned int {
-    INIT = 0,
-    BOOT,
-    SHOW_CO2,
-    SHOW_TEMPERATURE,
-    SLEEP
-};
-
-static const DisplayState nextState[] = {
-    /* INIT =>*/  BOOT,
-    /* BOOT =>*/  SHOW_CO2,
-    /* SHOW_CO2 =>*/SHOW_TEMPERATURE,
-    /* SHOW_TEMPERATURE =>*/  SLEEP,
-    /* SLEEP =>*/ SHOW_CO2
-};
-
-
-// we cycle the screen every 10 seconds, how many cycles should we show the same screen
-static const uint16_t screenCycles[] {
-    /* INIT */ 0,
-    /* BOOT */ 2,
-    /* SHOW_CO2 */1,
-    /* SHOW_TEMPERATURE*/1,
-    /* SLEEP*/2
-};
-
-
 Display::Display(Sensors *sensor, KiwiTimer &timer) : source{sensor}, visible{true},
-        state{INIT}, screenShownCount{0}  {
+        minutesPosition{0}, filled{false}  {
     #ifdef KIWI_SCREEN_64
     u8g2 = new U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0, U8X8_PIN_NONE, D1, D2);
     #else
@@ -37,46 +10,87 @@ Display::Display(Sensors *sensor, KiwiTimer &timer) : source{sensor}, visible{tr
     #endif
     u8g2->begin();
     EVERY(timer, 10*1000, Display, render);
+    EVERY(timer, PIXEL_DURATION, Display, updateValues);
+    for (uint16_t i = 0; i < PLOT_SIZE; i++) {
+        co2Values[i] = NAN;
+    }
 }
 
 bool Display::render() {
-    if (!visible) {
-        return true;
+    u8g2->clearBuffer();
+    if (source->getPresence() && visible) {
+        if (!filled && millis() < 30000) {
+            // draw logo
+            u8g2->drawXBM((128 - KIWI_LOGO_WIDTH) / 2, 0, KIWI_LOGO_WIDTH, KIWI_LOGO_HEIGHT, KIWI_LOGO);
+        } else {
+            displayValues();
+            plotGraph();
+        }
     }
-    screenShownCount++;
-    if (screenCycles[state] > screenShownCount) {
-        state = nextState[state];
-        screenShownCount = 0;
-    }
-    renderScreen(state);
+    u8g2->sendBuffer();
     return true;
 }
 
 void Display::setVisible(bool show) {
     visible = show;
-    screenShownCount = UINT16_MAX;
 }
 
-
-void Display::renderScreen(DisplayState state) {
-    u8g2->clearBuffer();
-    switch (state)
-    {
-    case BOOT:
-        u8g2->drawXBM((128 - KIWI_LOGO_WIDTH) / 2, 0, KIWI_LOGO_WIDTH, KIWI_LOGO_HEIGHT, KIWI_LOGO);
-        break;
-    case SHOW_TEMPERATURE:
-        u8g2->setFont(u8g2_font_logisoso26_tf);	// choose a suitable font
-        u8g2->setCursor(0,32);
-        u8g2->printf("%.1f\xb0%.1f%%", source->getTemperature(), source->getHumidity());
-        break;
-    case SHOW_CO2:
-        u8g2->setFont(u8g2_font_logisoso28_tf);	// choose a suitable font
-        u8g2->setCursor(0,32);
-        u8g2->printf("C: %.0f", source->getCO2());
-        break;
-    case SLEEP:
-        break;
+bool Display::updateValues() {
+    double value = source->getCO2();
+    if (isnan(value)) {
+        filled = false;
+        return true;
     }
-    u8g2->sendBuffer();
+    co2Values[minutesPosition % PLOT_SIZE] = value;
+    minutesPosition++;
+    filled = filled || minutesPosition >= PLOT_SIZE;
+    return true;
+}
+
+constexpr u8g2_uint_t GRAPH_X_OFFSET = 128 - PLOT_SIZE;
+
+#define GET_VALUE(__i) (co2Values[(minutesPosition - (PLOT_SIZE - (__i))) % PLOT_SIZE])
+
+void Display::displayValues() {
+    u8g2->setFont(u8g2_font_logisoso16_tf);
+    u8g2->setCursor(0,16);
+    if (source->hasCO2()) {
+        u8g2->printf("C: %.0f", source->getCO2());
+    }
+    else {
+        u8g2->print("CO2: ??");
+    }
+    u8g2->setCursor(0,32);
+    if (source->hasTemperature()) {
+        u8g2->printf("%.1f\xb0", source->getTemperature());
+    }
+    if (source->hasHumidity()) {
+        u8g2->printf("%.1f%%", source->getHumidity());
+    }
+}
+
+void Display::plotGraph() {
+    double rangeStart = GET_VALUE(0);
+    double rangeEnd = rangeStart;
+    for (u8g2_uint_t i = 1; i < PLOT_SIZE; i++) {
+        double value = GET_VALUE(i);
+        if (isnan(value)) {
+            continue;
+        } else if (value < rangeStart) {
+            rangeStart = value;
+        } else if (value > rangeEnd) {
+            rangeEnd = value;
+        }
+    }
+    double range = rangeEnd - rangeStart;
+    if (range < 100) {
+        range = 100;
+    }
+    for (u8g2_uint_t i = 0; i < PLOT_SIZE; i++) {
+        double value = GET_VALUE(i);
+        if (!isnan(value)) {
+            double y = ((value - rangeStart) / range) * PLOT_SIZE;
+            u8g2->drawVLine(GRAPH_X_OFFSET + i, (u8g2_uint_t)round(y), PLOT_SIZE);
+        }
+    }
 }
